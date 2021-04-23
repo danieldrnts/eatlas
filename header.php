8d9a26a0a1e76f0cf22352df30c7aa916850aa25
Daniel Dorantes <daniel.drnts@gmail.com>
2021-04-23 09:47:24 -0500

    Menu quick fixes
    TODO: Fix navbar container height in every device

--- header.php
+++ header.php
@@ -28,17 +28,19 @@
 	<div class="container-fluid  mx-0 px-0">
 
 		<!-- Navbar -->
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-			<div class="container-fluid bg-blue-3 ">
+		<nav class="navbar navbar-expand-lg navbar-light bg-blue-3">
+			<div class="container-fluid px-0 py-2">
 				<a class="navbar-brand" href="#">
 					<img src="https://play-lh.googleusercontent.com/IlyM8GsSE1CNeivAVhEEyFvRy9L_XvRGANeRdZBHYPu_tqFf3SK3rMietjfpZkxPxHDh=s180-rw" alt="" width="30" height="24" class="d-inline-block align-text-top">
 					eAtlas
 				</a>
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+				<button class="navbar-toggler me-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
 
-				<div class="collapse navbar-collapse" id="navbarText">
+				<div class="collapse navbar-collapse bg-blue-5 mt-2" id="navbarText" >
+
+
 					<!-- NOTE desktop MENU -->
 					<?php
 					wp_nav_menu(
@@ -49,40 +51,46 @@
 						)
 					);
 
+
+					?>
+
+					<p class="h1 text-white fw-bold mt-3 mb-4 ms-4">
+						<span class="under-line">More to see</span>
+					</p>
+
+					<?php
 					// Mobile nav
 					wp_nav_menu(
 						array(
 							'theme_location' => 'main-menu',
-							'menu_class' => 'navbar-nav  ps-4 pt-4',
-							'container_class' => 'container-fluid my-3 d-sm-none p-0 m-0 navbar-full-height d-flex flex-column justify-content-between mobile-child-custom',
+							'menu_class' => 'navbar-nav  ps-4 ',
+							'container_class' => 'container-fluid my-3 d-sm-none p-0 m-0 navbar-full-height  mobile-child-custom',
 						)
 					);
-
 					?>
-			
-					<!-- MOBILE SOCIAL ICONS -->
-					<div class="d-sm-none navbar-text  me-5">
-					<p class="text-white fw-light fs-5 text-center"> Our social media</p>
 
+					<!-- MOBILE SOCIAL ICONS -->
+					<div class="d-sm-none navbar-text ">
+						<p class="text-white fw-light fs-5 text-center"> Our social media</p>
 						<ul class="d-flex flex-row justify-content-center ps-0" style="list-style-type: none">
 							<li class="mx-2">
 								<a href="#">
-									<i class="bi bi-youtube blue-5" style="font-size: 1.5rem;"></i>
+									<i class="bi bi-youtube text-white" style="font-size: 1.5rem;"></i>
 								</a>
 							</li>
 							<li class="mx-2">
 								<a href="#">
-									<i class="bi bi-twitter blue-5" style="font-size: 1.5rem;"></i>
+									<i class="bi bi-twitter text-white" style="font-size: 1.5rem;"></i>
 								</a>
 							</li>
 							<li class="mx-2">
 								<a href="#">
-									<i class="bi bi-instagram blue-5" style="font-size: 1.5rem;"></i>
+									<i class="bi bi-instagram text-white" style="font-size: 1.5rem;"></i>
 								</a>
 							</li>
 							<li class="mx-2">
 								<a href="#">
-									<i class="bi bi-facebook blue-5" style="font-size: 1.5rem;"></i>
+									<i class="bi bi-facebook text-white" style="font-size: 1.5rem;"></i>
 								</a>
 							</li>
 						</ul>
