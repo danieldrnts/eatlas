31283a31d4ef2daeb88674de48358384fdc9ccec
Daniel Dorantes <daniel.drnts@gmail.com>
2021-04-23 23:02:03 -0500

    Landing ready

--- footer.php
+++ footer.php
@@ -10,8 +10,10 @@
  * @since 1.0.0
  */
 ?>
-		<h1>Footer </h1>
+		<!-- <h1>Footer </h1> -->
 
+		</div>
+		</div>
 	<!-- end tag para el main container -->
 	</div>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
--- front-page.php
+++ front-page.php
@@ -4,7 +4,30 @@ get_header();
 
 ?>
 
-<h1>Front page</h1>
+<div class="container-fluid px-0">
+    <div class="row mx-0">
+        <!-- NOTE mobile landing -->
+        <div class="col-sm-12 px-0">
+            <div class="d-flex flex-column" >
+                <p class="fs-1 blue-5 fw-bold text-center mt-4 airplane-svg">Explore</p> 
+                <p class="fs-1 blue-5 fw-bold text-center  airplane-svg"> the world</p>
+                <img style="height: 3em;" src="<?php echo get_bloginfo('template_url') ?>/assets/images/airplane.svg" ?>
+            </div>
+            <p class="fs-4 blue-5 fw-light text-center my-5">Live amazing Experiences</p>
+            <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/home-landing-bg.svg" alt="">
+        </div>
+    </div>
+    <div class="row mx-0">
+        <!-- NOTE mobile landing -->
+        <div class="container">
+            <div class="row col-12">
+                <p class="fw-bold fs-1">Meeet eAtlas</p>
+            </div>
+            <div class="row col-12"></div>
+            <div class="row col-12"></div>
+        </div>
+    </div>
+</div>
 
 
 <?php get_footer(); ?>
