803881445ebca23115ccffc20c00af83403d496a
Daniel Dorantes <daniel.drnts@gmail.com>
2021-04-26 19:29:01 -0500

    Video and Custemers/Builders

--- functions.php
+++ functions.php
@@ -1,24 +1,38 @@
 <?php
 
-	// Incluir mi Master.css
-	wp_register_style('master', get_template_directory_uri(). '/assets/css/master.css', array(), 
-		false, 'all');
-	wp_enqueue_style('master');
+// Incluir mi Master.css
+wp_register_style(
+	'master',
+	get_template_directory_uri() . '/assets/css/master.css',
+	array(),
+	false,
+	'all'
+);
 
-	        // Theme options
-			add_theme_support( 'menus' );
+// Incluir mi Master.js
+wp_register_script('master-js', get_template_directory_uri() . '/assets/js/master.js', array(), false, );
 
-			// Menus
-			register_nav_menus( 
-				array( 'main-menu' => 'Main Menu',
-					   'daniel-mobile-menu' => 'Mobile Daniel Menu' )
-			);
-		
-			/* Add custom classes to list item "li" */
-		
-		function _namespace_menu_item_class( $classes, $item ) {       
-			$classes[] = "nav-item"; // you can add multiple classes here
-			return $classes;
-		} 
-		
-		add_filter( 'nav_menu_css_class' , '_namespace_menu_item_class' , 10, 2 );
+wp_enqueue_style('master');
+wp_enqueue_script('master-js');
+// add_action('wp_enqueue_scripts', 'master-js');
+
+// Theme options
+add_theme_support('menus');
+
+// Menus
+register_nav_menus(
+	array(
+		'main-menu' => 'Main Menu',
+		'daniel-mobile-menu' => 'Mobile Daniel Menu'
+	)
+);
+
+/* Add custom classes to list item "li" */
+
+function _namespace_menu_item_class($classes, $item)
+{
+	$classes[] = "nav-item"; // you can add multiple classes here
+	return $classes;
+}
+
+add_filter('nav_menu_css_class', '_namespace_menu_item_class', 10, 2);
