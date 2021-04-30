dbadb0627caf8201fe2e929225eaea923461e498
Daniel Dorantes <daniel.drnts@gmail.com>
2021-04-30 13:35:56 -0500

    Details to meet builders mobile

--- template-parts/builder-card.php
+++ template-parts/builder-card.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 /**
  * @var array builderInfo
  */
@@ -6,30 +7,33 @@
 ?>
 
 <div class="container-fluid h-100 flex-grow-1 d-flex flex-column justify-content-center">
-<div class="card h-100 custom-border" style="width: 18rem;">
-  <div class="card-body">
-      <div class="rounded-circle bg-warning d-flex flex-row justify-content-center mb-3">
-      <i style="font-size: 10em;" class="bi bi-person-circle"></i>
+      <div class="card h-100 custom-border " style="width: 18rem;" >
+      <!-- <div style="height: 10em; width: 100%" class="position-absolute bg-warning ">
+      
+      </div> -->
+            <div class="card-body" style="z-index: 10;">
+                  <div style="height: 10em;" class=" d-flex flex-row justify-content-center mb-1">
+                        <i style="font-size: 7em;" class="bi bi-person-circle"></i>
+                  </div>
+                  <h5 class="card-title text-center mb-4">Builder name here</h5>
+                  <div>
+                        <ul style="list-style: none;">
+                              <li class="my-2">
+                                    <i class="bi bi-heart-fill blue-5"></i>
+                                    <span class="fs-6 fw-normal blue-5">Builder City</span>
+                              </li>
+                              <li class="my-2">
+                                    <i class="bi bi-heart-fill blue-5"></i>
+                                    <span class="fs-6 fw-normal blue-5">3</span>
+                              </li>
+                              <li class="my-2">
+                                    <i class="bi bi-heart-fill blue-5"></i>
+                                    <span class="fs-6 fw-normal blue-5">6</span>
+                              </li>
+                        </ul>
+                  </div>
+                  <p class="blue-4 card-text text-center fst-italic " style="font-size: small;">With supporting text below as a natural lead-in to additional content. a natural lead-in to additional content. a natural lead-in to additional content.</p>
+                  <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+            </div>
       </div>
-    <h5 class="card-title text-center">Builder name here</h5>
-    <div>
-        <ul style="list-style: none;">
-            <li>
-            <i class="bi bi-heart-fill blue-5"></i>
-      <span class="fs-6 fw-normal blue-5">Builder City</span>
-            </li>
-            <li>
-            <i class="bi bi-heart-fill blue-5"></i>
-      <span class="fs-6 fw-normal blue-5">3</span>
-            </li>
-            <li>
-            <i class="bi bi-heart-fill blue-5"></i>
-      <span class="fs-6 fw-normal blue-5">6</span>
-            </li>
-        </ul>
-    </div>
-    <p class="card-text text-center fst-italic">With supporting text below as a natural lead-in to additional content.</p>
-    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-  </div>
-</div>
 </div>
