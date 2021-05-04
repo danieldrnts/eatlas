f42feb714ae95c5180779e27badffe541c96a69a
Daniel Dorantes <daniel.drnts@gmail.com>
2021-05-04 14:33:08 -0500

    quick fixes to Builders / consumers

--- front-page.php
+++ front-page.php
@@ -99,10 +99,11 @@ get_header();
                     </div>
                 </div>
                 <div class="col-12 col-md-6">
-                    <img class="img-fluid" src="<?php echo get_bloginfo('template_url') ?>/assets/images/womanandgirl.svg" alt="womanandgirl">
+                    <img class="img-fluid d-none d-md-block " src="<?php echo get_bloginfo('template_url') ?>/assets/images/consumers.svg" alt="consumers">
+                    <img class="img-fluid d-md-none" src="<?php echo get_bloginfo('template_url') ?>/assets/images/womanandgirl.svg" alt="womanandgirl">
                 </div>
             </div>
-            <div class="row bg-blue-4 pt-3 mt-md-4 pt-md-5">
+            <div class="row bg-blue-4 pt-3  pt-md-5">
                 <div class="col-12 d-md-none">
                     <p class="fw-bold fs-1  text-center text-white">Builders</p>
                     <p class="fw-normal fs-5 text-center text-white">Lorem ipsum dolor sit amet</p>
@@ -173,7 +174,7 @@ get_header();
                             <path class="cls-1" d="M490.69,1616.74s-28.88-79-76.85-112.88c0,0-22.32,160.71-52,154.71s37.78-177.29,37.78-177.29S281.86,1366.54,225,1353.35c0,0,37.56,176.87,10.3,163.76s-74.9-195.34-74.9-195.34S61,1266.54,0,1299.84v185.41c46.65,18.47,118.79,45.51,111.8,57.67s-63.8,14-111.8,1V1673c10.91,13.91,24.34,24.28,40.86,28.13,77.08,18,115.72-20.51,163.15-8.53s77,35.76,49.74,40.45S151,1703.1,103,1722.31s-84.81,28.62-20.23,66.13,192,70.43,234.84,25.44h0l5.35-5.62,0,0a29.81,29.81,0,0,1-5.39,5.68c10.29,11.76,17.52,53.82,21.67,85.6.33,5.26,1.17,12.68,2.69,22.84.5,4.71.87,8.54,1.12,11.15,1.12,20.54-2.32,43.86-7.74,66.52h20.57c6.55-51-3.49-89.18-3.53-89.34h0c-14.05-81.9,8-141.76,8-141.76h0C360.79,1768.83,518.86,1738.87,490.69,1616.74Z" transform="translate(0 -256.17)" />
                             <path class="cls-1" d="M1812.44,1475.49c-27.11-100.13-196.08-68.84-196.08-68.84s-114.73,175.23-148.11,179.4,68.84-169,68.84-169c-64.67-6.25-229.46,73-229.46,73s10.43,204.43-23,200.26,0-181.48,0-181.48c-62.58,18.77-121,91.78-121,91.78-73,118.9,83.44,206.52,83.44,206.52s2.09,70.92-41.71,152.28h0s-9.88,15-19.93,40.55h17.06q2.09-6.87,4.63-13.24a.1.1,0,0,1,0-.08,1.42,1.42,0,0,1,.08-.18c1.16-2.64,2.84-6.39,4.94-11v0c5.21-10.2,8.73-17.74,11-23.18a.11.11,0,0,0,0,0c15.62-32,38.12-73.77,53.13-82.52a33,33,0,0,1-3.65-7.83c1.16,2.5,2.38,5.1,3.65,7.83,29.2,62.58,175.22,73,256.58,56.32s45.89-39.63,2.08-76.88-135.59-27.42-162.7-42,12.51-29.21,66.75-25,81.35,58.41,169,66.76,154.36-156.45,154.36-156.45c-60.49,18.77-169-14.61-173.14-35.47s156.45-18.77,181.48-25S1839.56,1575.62,1812.44,1475.49Z" transform="translate(0 -256.17)" />
                         </svg> -->
-                        <img class="img-fluid" src="<?php echo get_bloginfo('template_url') ?>/assets/images/hand-and-phone.svg" alt="">
+                        <img class="img-fluid hand-and-phone-svg" src="<?php echo get_bloginfo('template_url') ?>/assets/images/hand-and-phone.svg" alt="">
                     </div>
                 </div>
                 <div class=" d-none d-md-block col-12 col-md-6 ">
@@ -253,7 +254,7 @@ get_header();
             <p class="fw-light my-1 fs-6 text-white my-3 mx-5 text-center"> Lorem ipsum dolor sit amet dolor sit </p>
         </div>
     </div>
-    <!-- NOTE Some Experiences -->
+    <!-- NOTE Some Experiences mobile-->
     <div class="row m-0 p-0 bg-blue-2 d-flex flex-column d-md-none" style="min-height: 100vh;">
         <div class="col-12 mt-3">
             <p class="fw-bold blue-5 fs-1 text-center">Some experiences</p>
@@ -291,77 +292,88 @@ get_header();
             </p>
         </div>
     </div>
-    <!-- NOTE Some Experiences -->
+    <!-- NOTE Some Experiences desktop -->
+    <!-- TODO terminar la galleria -->
     <div class="row m-0 p-0 d-none d-md-flex" style="min-height: 100vh;">
-        <div class="col-8 bg-blue-1">
-            <div class="container-fluid ">
-                <div class="row m-1 bg-blue-2">
+        <div class="col-8 bg-blue-1" style="min-height: 100%;">
+            <div class="container-fluid b " style="min-height: 100%;">
+
+                <div class="row m-1 bg-blue-2" style="min-height: 20%;">
                     <div class="col-5">
-                        <div class="card bg-dark text-white">
-                            <img src="https://source.unsplash.com/random/500x300" class="card-img" alt="...">
+                        <h1>1</h1>
+                        <!-- <div class="card bg-dark text-white">
+                            <img src="https://source.unsplash.com/random/" class="card-img" alt="...">
                             <div class="card-img-overlay">
                                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                 <p class="card-text">Last updated 3 mins ago</p>
                             </div>
-                        </div>
+                        </div> -->
                     </div>
                     <div class="col-7">
-                    <div class="card bg-dark text-white">
-                            <img src="https://source.unsplash.com/random/500x300" class="card-img" alt="...">
+                        <h1>2</h1>
+                        <!-- <div class="card bg-dark text-white">
+                            <img src="https://source.unsplash.com/random/" class="card-img"  alt="...">
                             <div class="card-img-overlay">
                                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                 <p class="card-text">Last updated 3 mins ago</p>
                             </div>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
-                <div class="row m-1 bg-blue-3">
+
+                <div class="row m-1 bg-blue-3" style="min-height: 50%;">
                     <div class="col-5">
-                    <div class="card bg-dark text-white">
+                        <h1>3</h1>
+                        <!-- <div class="card bg-dark text-white">
                             <img src="https://source.unsplash.com/random/500x300" class="card-img" alt="...">
                             <div class="card-img-overlay">
                                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                 <p class="card-text">Last updated 3 mins ago</p>
                             </div>
-                        </div>
+                        </div> -->
                     </div>
                     <div class="col-2">
-                    <div class="card bg-dark text-white">
+                        <h1>4</h1>
+                        <!-- <div class="card bg-dark text-white">
                             <img src="https://source.unsplash.com/random/500x300" class="card-img" alt="...">
                             <div class="card-img-overlay">
                                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                 <p class="card-text">Last updated 3 mins ago</p>
                             </div>
-                        </div>
+                        </div> -->
                     </div>
                     <div class="col-5">
-                    <div class="card bg-dark text-white">
+                        <h1>5</h1>
+                        <!-- <div class="card bg-dark text-white">
                             <img src="https://source.unsplash.com/random/500x300" class="card-img" alt="...">
                             <div class="card-img-overlay">
                                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                 <p class="card-text">Last updated 3 mins ago</p>
                             </div>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
-                <div class="row m-1 bg-blue-2">
+                <div class="row m-1 bg-blue-2" style="min-height: 30%;">
                     <div class="col-7">
-                    <div class="card bg-dark text-white">
+                    <h1>6</h1>
+                        <!-- <div class="card bg-dark text-white">
                             <img src="https://source.unsplash.com/random/500x300" class="card-img" alt="...">
                             <div class="card-img-overlay">
                                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                 <p class="card-text">Last updated 3 mins ago</p>
                             </div>
-                        </div>
+                        </div> -->
                     </div>
                     <div class="col-5">
-                    <div class="card bg-dark text-white">
+                    <h1>7</h1>
+
+                        <!-- <div class="card bg-dark text-white">
                             <img src="https://source.unsplash.com/random/500x300" class="card-img" alt="...">
                             <div class="card-img-overlay">
                                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                 <p class="card-text">Last updated 3 mins ago</p>
                             </div>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
             </div>
