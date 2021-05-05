34af0165a8a1afa9ed595106e7e34f5ddbafc49c
Daniel Dorantes <daniel.drnts@gmail.com>
2021-05-04 22:10:59 -0500

    Builders desktop

--- pages/builders.php
+++ pages/builders.php
@@ -5,23 +5,36 @@ get_header();
 ?>
 
 <div class="container-fluid p-0 m-0">
-    <div class="row mx-0">
-        <!-- NOTE mobile landing -->
-        <div class="col-sm-12 px-0">
+    <!-- NOTE mobile & desktop landing -->
+    <div class="row mx-0 d-md-flex flex-md-row">
+        <div class="col-sm-12 col-md-6 px-0">
             <div class="d-flex flex-column">
-                <p class="fs-1 blue-5 fw-bold text-center mt-3 airplane-svg">Become a</p>
-                <p class="fs-1 blue-5 fw-bold text-center  airplane-svg"> Builder</p>
-                <img style="height: 3em;" src="<?php echo get_bloginfo('template_url') ?>/assets/images/airplane.svg" ?>
+                <p class="d-md-none fs-1 blue-5 fw-bold text-center mt-3 airplane-svg">Become a</p>
+                <p class="d-md-none fs-1 blue-5 fw-bold text-center  airplane-svg"> Builder</p>
+                <div class="d-none d-md-flex flex-md-column  mt-5">
+                    <p class="fw-bold fs-1 pt-1 text-center blue-5">Get eAtlas</p>
+                    <p class="fw-light fs-4 text-center blue-5 my-4">Available on every device</p>
+                    <div class="d-flex flex-row  justify-content-center mx-auto">
+                        <img class="mx-2 shadow" style="height: 3em;" src="<?php echo get_bloginfo('template_url') ?>/assets/images/google-play-badge.svg" alt="google-badge">
+                        <img class="mx-2 shadow" style="height: 3em;" src="<?php echo get_bloginfo('template_url') ?>/assets/images/apple-badge.svg" alt="google-badge">
+                    </div>
+                </div>
+                <img class="mt-md-5" style="height: 3em;" src="<?php echo get_bloginfo('template_url') ?>/assets/images/airplane.svg" ?>
             </div>
-            <p class="fs-4 blue-5 fw-light text-center my-3">Create amazing Experiences today!</p>
-            <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/hand-and-phone.svg" alt="home landing ">
+
+        </div>
+        <div class="col-sm-12 col-md-6 radius-md-custom">
+            <p class="d-none d-md-block text-white blue-5 fw-bold fs-1 text-center mt-5">Become a Builder,</p>
+            <p class="d-none d-md-block text-white blue-5 fw-bold fs-1 text-center">and share your <span class="fw-bold">Experiences</span> </p>
+            <p class="d-md-none fs-4 blue-5 fw-light text-center my-3">Create amazing Experiences today!</p>
+            <img class="hand-and-phone-svg mx-md-auto" src="<?php echo get_bloginfo('template_url') ?>/assets/images/hand-and-phone.svg" alt="home landing">
         </div>
     </div>
 
     <!-- NOTE meet video -->
     <div class="row mx-0 bg-blue-1" style="height: 100vh;">
-        <!-- NOTE mobile landing -->
-        <div class="container ">
+        <!-- NOTE mobile video -->
+        <div class="container d-md-none">
             <div class="row col-12 py-4">
                 <p class="fw-normal fs-1 text-center blue-5">Builders, <br> <span class="fw-bolder">Whoa!</span> the world </p>
                 <p class="fw-lighter fs-5 text-center blue-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum quidem ullam odit enim quas rerum libero rem </p>
@@ -39,6 +52,28 @@ get_header();
                 </div>
             </div>
         </div>
+
+        <!-- NOTE desktop  video -->
+        <div class="d-none d-md-block container">
+            <div class="row">
+                <div class="col-6">
+                    <div class="row">
+                        <div class="col-12">
+                            <p class="d-none d-md-block text-white blue-5 fw-bold fs-1 text-center mt-5">Become a Builder,</p>
+                        </div>
+                        <div class="col-12 bg-blue-5">
+                            <video controls style="height: 3em" src="<?php echo get_bloginfo('template_url') ?>/assets/videos/meetVideo.mp4">
+                            </video>
+                        </div>
+                        <div class="col-12">
+                            <p class="d-none d-md-block text-white blue-5 fw-light fs-6 text-center mt-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur modi laudantium provident error, corrupti </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6"></div>
+            </div>
+        </div>
+
     </div>
 
     <!-- NOTE Be a builder -->
@@ -63,10 +98,10 @@ get_header();
                 <img class="mx-2 shadow" style="height: 3em;" src="<?php echo get_bloginfo('template_url') ?>/assets/images/apple-badge.svg" alt="google-badge">
             </div>
         </div>
-        <div class=" mt-5 col-12 bg-blue-4 d-flex flex-column justify-content-center flex-grow-1 " style="border-top-left-radius: 50%;border-top-right-radius: 50%;">
+        <div class="mt-5 col-12 bg-blue-4 d-flex flex-column justify-content-center flex-grow-1 " style="border-top-left-radius: 50%;border-top-right-radius: 50%;">
             <p class="fw-normal fs-2 text-white  mt-5 pt-2 mx-5 text-center">Experiences all around the <span class="fw-bold">globe</span> </p>
             <img class="mx-2" style="height: 12em;" src="<?php echo get_bloginfo('template_url') ?>/assets/images/world-map.svg" alt="google-badge">
-            <p class="fw-light my-1 fs-6 text-white my-3 mx-5 text-center"> Lorem ipsum dolor sit amet dolor sit </p>
+            <p class="fw-light my-1 fs-6 text-white my-3 mx-5 text-center"> Lorem ipsum dolor sit amet dolor sit</p>
         </div>
     </div>
 
